66520dd83992fd7182822e239bffdd57cb2986db
Vo Nhat Nam <vonhatnam1718@gmail.com>
2023-12-28 10:37:06 +0700

    register page 2

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,17 @@
 import { PATH } from "../config/path";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import Resgister2 from "../pages/Resgister2";
 
 const route = [
     {
         path: PATH.sign_up,
         component: Register,
+        exact: true,
+    },
+    {
+        path: PATH.sign_up2,
+        component: Resgister2,
         exact: true,
     },
     {
